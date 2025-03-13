8b09ec49923acdd409f04bb8a684176ab132776d
Rony2518 <ronyvdhimmar464@gmail.com>
2025-03-13 21:24:22 +0530

    Fix bug in User Model

--- Repositories/Models/User.cs
+++ Repositories/Models/User.cs
@@ -5,10 +5,9 @@ namespace Repositories.Models
 {
     public class User
     {
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
 
         [Required(ErrorMessage = "Role is required")]
-        [StringLength(1)]
         public char Role { get; set; } = 'E';
 
         [Required(ErrorMessage = "First name is required")]
@@ -32,7 +31,6 @@ namespace Repositories.Models
         [StringLength(15)]
         public string? Contact { get; set; }
 
-        [StringLength(1)]
         public char? Gender { get; set; }
 
         public string? Image { get; set; }
